f719abf3ea242a806d0faaf117fe3782b6481be2
Shir Zemour <shirzcode@gmail.com>
2024-01-31 17:49:30 +0200

    task router

--- index.js
+++ index.js
@@ -8,4 +8,7 @@ const cors = require('cors')
 app.use(cors())
 app.use(express.json())
 
+const taskRouter = require('./router/task.router')
+app.use('/user:id', taskRouter)
+
 app.listen(2500,()=>console.log("****The Server is UP****"));
